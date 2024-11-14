7ad91ab951b7d043794e8f30cc82e763d82f0911
Nicholas2k4 <nicholas.s2k4@gmail.com>
2024-11-14 18:28:40 +0700

    feat: register mentor middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\StudentMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,12 +13,12 @@ Route::get('/', function () {
  */
 Route::controller(AuthController::class)->group(function () {
     Route::get('/register', 'showRegister')->name('register');
-    Route::get('/register/mentor', 'showRegisterMentor')->name('register.mentor');
+    Route::get('/register/mentor', 'showRegisterMentor')->middleware([StudentMiddleware::class])->name('register.mentor');
     Route::get('/login', 'showLogin')->name('login');
     Route::get('/logout', 'logout')->name('logout');
 
     Route::post('/register', 'register')->name('register');
-    Route::post('/register/mentor', 'registerMentor')->name('register.mentor');
+    Route::post('/register/mentor', 'registerMentor')->middleware([StudentMiddleware::class])->name('register.mentor');
     Route::post('/login', 'login')->name('login');
 });
 
