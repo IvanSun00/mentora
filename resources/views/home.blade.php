b2227fcfae616ccfa5b0a5112af84624d321c037
Timothy VC <c14220197@john.petra.ac.id>
2024-11-28 13:43:47 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @endsection
 
 @section('content')
-<div class="p-10 max-h-screen">
+<div class="p-10 min-h-screen bg-gradient-to-r from-blue-300 to-purple-300 relative overflow-hidden">
     <!-- Logo and Text Content Row -->
     <div class="flex items-center justify-between mb-8 flex-row-reverse">
         <!-- Logo -->
@@ -13,20 +13,22 @@
 
         <!-- Text Content -->
         <div class="flex-1 p-10">
-            <h1 class="text-5xl font-bold mb-2">Find your <span class="text-black font-extrabold">mentors</span></h1>
-            <h2 class="text-5xl font-bold mb-2">Become <span class="text-black font-extrabold">mentor</span></h2>
-            <p class="text-2xl">Be the best version of yourself</p>
+            <h1 class="text-7xl font-bold mb-2">Find your <span class="text-black font-extrabold">mentors</span></h1>
+            <h2 class="text-7xl font-bold mb-2">Become <span class="text-black font-extrabold">mentor</span></h2>
+            <p class="text-4xl">Be the best version of yourself</p>
         </div>
     </div>
 
     <!-- Button -->
-    <div class="mb-8 ml-10">
-        <button class="bg-yellow-500 text-white text-lg font-semibold py-3 px-8 rounded-lg hover:bg-yellow-600 transition duration-300"><a href="{{ route('login') }}">Start Journey</a></button>
+    <div class="mb-8 ml-10 h-20 w-64">
+        <button class="bg-yellow-500 text-white text-2xl font-semibold py-3 px-8 rounded-lg hover:bg-yellow-600 transition duration-300 h-20 w-64">
+            <a href="{{ route('login') }}">Start Journey</a>
+        </button>
     </div>
 
     <!-- Illustration -->
-    <div class="flex justify-end object-none object-left-bottom w-full">
-        <img src="{{ asset('home.png') }}" alt="Mentorship illustration" class="w-[50%]">
+    <div class="flex justify-end object-none object-left-bottom w-full absolute bottom-[-64px] right-0 overflow-y-hidden">
+        <img src="{{ asset('home.png') }}" alt="Mentorship illustration" class="w-[65%]">
     </div>
 </div>
 @endsection
